74aede910b7c5a7f3da60b87b7f6c85cb57abbd8
Pawel Sobkowski <psobkowski98@gmail.com>
2020-10-13 17:58:27 +0200

    feat: add object to store using colors

---
+++ src/colors.js
@@ -0,0 +1,11 @@
+const COLORS = {
+  black087: "rgba(0,0,0,0.87)",
+  black063: "rgba(0,0,0,0.63)",
+  backgroundColor: "#FFF",
+  borderColor: "#EBEBEB",
+  labelColor: "#C2C2C2",
+  primaryColor: "#1BA0EB",
+  googleButtonColor: "#CDCDCD",
+};
+
+export default COLORS;
