3d78b04119812e767c5ba5f1e931439036ae9040
Pawel Sobkowski <psobkowski98@gmail.com>
2020-10-14 11:27:24 +0200

    feat: can choose between sign in and sign up view

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
 
 function App() {
-  return <SignUp></SignUp>;
+  const [loginView, setLoginView] = useState("signUp");
+  return loginView === "signUp" ? (
+    <SignUp changeLoginView={() => setLoginView("signIn")} />
+  ) : (
+    <SignIn changeLoginView={() => setLoginView("signUp")} />
+  );
 }
 
 export default App;
