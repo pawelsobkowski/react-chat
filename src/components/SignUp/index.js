82f844e9e1ff77fed55f456abb4c8aa39fc8c8d5
Pawel Sobkowski <psobkowski98@gmail.com>
2020-10-14 11:26:05 +0200

    feat: add hook to controll input value

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,7 +1,10 @@
-import React from "react";
-import Styled from "./style";
+import React, { useState } from "react";
+import Styled from "../../sharedStyles/loginViewStyle";
+import { useInputChange } from "../hooks/useInputChange";
 
-const SignUp = () => {
+const SignUp = ({ changeLoginView }) => {
+  const [input, setInputChange] = useInputChange();
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
   return (
     <Styled.Container>
       <Styled.Header>
@@ -16,18 +19,33 @@ const SignUp = () => {
           <Styled.Input
             type="text"
             id="fullName"
-            name="fullName"></Styled.Input>
+            name="fullName"
+            onChange={setInputChange}></Styled.Input>
         </Styled.InputContainer>
         <Styled.InputContainer>
           <Styled.Label htmlFor="email">Email</Styled.Label>
-          <Styled.Input type="email" id="email" name="email"></Styled.Input>
+          <Styled.Input
+            type="email"
+            id="email"
+            name="email"
+            onChange={setInputChange}></Styled.Input>
         </Styled.InputContainer>
         <Styled.InputContainer>
           <Styled.Label htmlFor="password">Password</Styled.Label>
           <Styled.Input
-            type="password"
+            type={isPasswordVisible ? "text" : "password"}
             id="password"
-            name="password"></Styled.Input>
+            name="password"
+            onChange={setInputChange}></Styled.Input>
+          {isPasswordVisible ? (
+            <Styled.EyeIcon
+              onClick={() => setIsPasswordVisible(!isPasswordVisible)}
+            />
+          ) : (
+            <Styled.EyeOffIcon
+              onClick={() => setIsPasswordVisible(!isPasswordVisible)}
+            />
+          )}
         </Styled.InputContainer>
         <Styled.Button type="submit">Sign up</Styled.Button>
         <Styled.Button type="submit" buttonType={"Google"}>
@@ -37,7 +55,7 @@ const SignUp = () => {
       </Styled.Form>
       <p>
         I'm already a member,
-        <Styled.Link href="//#endregion">
+        <Styled.Link onClick={changeLoginView}>
           <strong> sign in</strong>
         </Styled.Link>
       </p>
