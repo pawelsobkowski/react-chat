b83c95960308894818c41f82497844e5177bba78
Pawel Sobkowski <psobkowski98@gmail.com>
2020-10-14 17:29:26 +0200

    Updates

---
+++ precache-manifest.60993b940282363517b057e6369d8b3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ac4fcf6dfc7c25b42841e3bb18af832",
+    "url": "/react-chat/index.html"
+  },
+  {
+    "revision": "e810af3d7372444cdf00",
+    "url": "/react-chat/static/css/main.8e53cb4c.chunk.css"
+  },
+  {
+    "revision": "15f77384b6ac2dffdc2b",
+    "url": "/react-chat/static/js/2.ebf91c0e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-chat/static/js/2.ebf91c0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e810af3d7372444cdf00",
+    "url": "/react-chat/static/js/main.8e00a15c.chunk.js"
+  },
+  {
+    "revision": "21fff9b89844a8f72f87",
+    "url": "/react-chat/static/js/runtime-main.6fc5081d.js"
+  },
+  {
+    "revision": "02d31de090b561394cf0e18c8419927d",
+    "url": "/react-chat/static/media/google.02d31de0.svg"
+  }
+]);
