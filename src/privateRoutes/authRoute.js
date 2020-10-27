edda68d1b0b2f2307db21c3d1e6fc04caf02dd1d
Pawel Sobkowski <psobkowski98@gmail.com>
2020-10-27 15:34:50 +0100

    feat: create private routes

---
+++ src/privateRoutes/authRoute.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+
+const AuthRoute = ({ children, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      localStorage.getItem("token") ? <Redirect to="/chats" /> : children
+    }
+  />
+);
+
+export default AuthRoute;
