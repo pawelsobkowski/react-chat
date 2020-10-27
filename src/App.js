75154b3f64969f41d987a2989317fdb456327093
Pawel Sobkowski <psobkowski98@gmail.com>
2020-10-27 15:36:47 +0100

    feat: use private routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { useState } from "react";
 import SignUp from "./components/SignUp";
 import SignIn from "./components/SignIn";
+import { HashRouter, Switch } from "react-router-dom";
+import ProtectedRoute from "./privateRoutes/protectedRoute";
+import AuthRoute from "./privateRoutes/authRoute";
+import Chats from "./components/Chats";
 
 function App() {
   const [loginView, setLoginView] = useState("signUp");
-  return loginView === "signUp" ? (
-    <SignUp changeLoginView={() => setLoginView("signIn")} />
-  ) : (
-    <SignIn changeLoginView={() => setLoginView("signUp")} />
+  return (
+    <HashRouter>
+      <Switch>
+        <AuthRoute exact path="/">
+          {loginView === "signUp" ? (
+            <SignUp changeLoginView={() => setLoginView("signIn")} />
+          ) : (
+            <SignIn changeLoginView={() => setLoginView("signUp")} />
+          )}
+        </AuthRoute>
+        <ProtectedRoute exact path="/chats" component={Chats} />
+      </Switch>
+    </HashRouter>
   );
 }
 
