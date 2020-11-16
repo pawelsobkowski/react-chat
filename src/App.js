6123b1c9a71c951de63ab100d55486f15377bc1e
Pawel Sobkowski <psobkowski98@gmail.com>
2020-11-16 18:55:01 +0100

    feat: use contacts component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { HashRouter, Switch } from "react-router-dom";
 import ProtectedRoute from "./privateRoutes/protectedRoute";
 import AuthRoute from "./privateRoutes/authRoute";
 import Chats from "./components/Chats";
+import Contacts from "./components/Contacts";
 
 function App() {
   const [loginView, setLoginView] = useState("signUp");
@@ -19,6 +20,7 @@ function App() {
           )}
         </AuthRoute>
         <ProtectedRoute exact path="/chats" component={Chats} />
+        <ProtectedRoute exact path="/contacts" component={Contacts} />
       </Switch>
     </HashRouter>
   );
