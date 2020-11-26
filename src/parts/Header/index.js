346ee46b8a0b27761177ba86e82850f0710db199
Pawel Sobkowski <psobkowski98@gmail.com>
2020-11-26 23:20:55 +0100

    feat: add search function to search input

--- src/parts/Header/index.js
+++ src/parts/Header/index.js
@@ -1,17 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Styled from "./style";
+import Search from "../Search";
+import axios from "axios";
+import parseJwt from "../../functions/parseJWT";
 
 const Header = ({ title, logout }) => {
+  const [isSearchActive, setIsSearchActive] = useState(false);
+  const [searchResults, setSearchResults] = useState([]);
+  const { userId } = parseJwt();
+
+  const search = async (value) => {
+    const res = await axios.post(`http://localhost:3001/searchUsers`, {
+      searchValue: value,
+      userId,
+    });
+    setSearchResults(res.data);
+  };
+
   return (
     <Styled.Header>
       <Styled.TopSection>
         <Styled.Title>{title}</Styled.Title>
         <button onClick={logout}>Logout</button>
       </Styled.TopSection>
-      <Styled.SearchBar>
+      <Styled.SearchBar isSearchActive={isSearchActive}>
         <Styled.SearchIcon />
-        <Styled.Search type="search" placeholder="Search" />
+        <Styled.Search
+          type="search"
+          placeholder="Search"
+          isSearchActive={isSearchActive}
+          onClick={() => setIsSearchActive(true)}
+          onChange={(e) => search(e.currentTarget.value)}
+        />
+        {isSearchActive && (
+          <Styled.CancelButton onClick={() => setIsSearchActive(false)}>
+            Cancel
+          </Styled.CancelButton>
+        )}
       </Styled.SearchBar>
+      {isSearchActive && <Search results={searchResults} />}
     </Styled.Header>
   );
 };
