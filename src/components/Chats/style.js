48dd2d5a58455d4b899449dda1b2766ae8564542
Pawel Sobkowski <psobkowski98@gmail.com>
2020-11-29 22:58:50 +0100

    style: add styles for chat components

--- src/components/Chats/style.js
+++ src/components/Chats/style.js
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 import COLORS from "../../colors";
+import { Link } from "react-router-dom";
 
 const Container = styled.div`
   width: 100%;
   height: 100vh;
-  padding: 1em;
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
@@ -12,8 +12,71 @@ const Container = styled.div`
   padding: 0;
 `;
 
+const List = styled.ul`
+  width: 100%;
+  list-style: none;
+  margin: 0;
+  padding: 0.5em 1em;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ListElement = styled.li`
+  width: 100%;
+  &:first-child {
+    margin-top: 0;
+  }
+  &:last-child {
+    margin-bottom: 0;
+  }
+`;
+
+const LinkToChat = styled(Link)`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0.5em 0;
+  text-decoration: none;
+  color: ${COLORS.black087};
+`;
+
+const UserName = styled.span`
+  grid-area: name;
+  font-weight: 500;
+`;
+
+const Message = styled.span`
+  grid-area: message;
+  color: ${COLORS.black063};
+  font-size: 0.9em;
+`;
+
+const Timestamp = styled.span`
+  grid-area: timestamp;
+  color: ${COLORS.black063};
+  font-size: 0.9em;
+`;
+
+const ChatInfoSection = styled.section`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr auto;
+  grid-template-rows: auto;
+  grid-template-areas:
+    "name name"
+    "message timestamp";
+`;
+
 const Styled = {
   Container,
+  List,
+  ListElement,
+  UserName,
+  Message,
+  Timestamp,
+  ChatInfoSection,
+  LinkToChat,
 };
 
 export default Styled;
