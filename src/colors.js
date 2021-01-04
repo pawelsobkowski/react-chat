808643ba2762320ebc77958aab32f7462b72f613
Pawel Sobkowski <psobkowski98@gmail.com>
2021-01-04 19:32:29 +0100

    feat: add new colors

--- src/colors.js
+++ src/colors.js
@@ -1,15 +1,19 @@
 const COLORS = {
   black087: "rgba(0,0,0,0.87)",
   black063: "rgba(0,0,0,0.63)",
-  backgroundColor: "#FFF",
+  backgroundColor: "#fafafa",
   borderColor: "#EBEBEB",
   labelColor: "#C2C2C2",
   primaryColor: "#1BA0EB",
   lightPrimaryColor: "#0ECFFA",
   googleButtonColor: "#CDCDCD",
   error: "#CC0000",
-  transparentBgColor: "rgba(255,255,255,0.67)",
+  transparentBgColor: "rgba(250,250,250,0.67)",
   transparentSearchBgColor: "rgba(230,230,230,0.87)",
+  hoverColor: "#f6f6f6",
+  accountColorIcon: "#15e1d5",
+  deleteColorIcon: "#e11515",
+  neutralColorIcon: "#c1c7c9",
 };
 
 export default COLORS;
