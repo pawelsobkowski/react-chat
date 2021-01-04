92ab2deda50dcad89a2c5547d8958c209b0cab02
Pawel Sobkowski <psobkowski98@gmail.com>
2021-01-04 19:33:26 +0100

    feat: create Profile component

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,7 +1,56 @@
 import React, { useEffect } from "react";
+import Avatar from "../../parts/Avatar";
+import Styled from "../Profile/style";
+import { useHistory } from "react-router-dom";
 
 const Profile = () => {
-  return <div></div>;
+  const history = useHistory();
+
+  const goBack = () => {
+    history.goBack();
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    history.push("/");
+  };
+
+  return (
+    <Styled.Container>
+      <Styled.Header>
+        <Styled.Title>Profile</Styled.Title>
+        <Styled.Close onClick={goBack}>Done</Styled.Close>
+      </Styled.Header>
+      <Avatar
+        photoUrl={"https://source.unsplash.com/128x128/?people"}
+        size={128}
+      />
+      <Styled.Name>Joe Johnson</Styled.Name>
+      <Styled.List>
+        <Styled.SectionTitle>Account</Styled.SectionTitle>
+        <Styled.ListElement>
+          <Styled.PersonIcon />
+          <Styled.Option>Personal info</Styled.Option>
+          <Styled.ChevronRightIcon />
+        </Styled.ListElement>
+        <Styled.ListElement>
+          <Styled.LockIcon />
+          <Styled.Option>Password</Styled.Option>
+          <Styled.ChevronRightIcon />
+        </Styled.ListElement>
+        <Styled.ListElement>
+          <Styled.DeleteIcon />
+          <Styled.Option>Delete Account</Styled.Option>
+          <Styled.ChevronRightIcon />
+        </Styled.ListElement>
+        <Styled.ListElement onClick={logout}>
+          <Styled.LogOutIcon />
+          <Styled.Option>Logout</Styled.Option>
+          <Styled.ChevronRightIcon />
+        </Styled.ListElement>
+      </Styled.List>
+    </Styled.Container>
+  );
 };
 
 export default Profile;
---
+++ src/components/Profile/style.js
@@ -0,0 +1,152 @@
+import styled from "styled-components";
+import COLORS from "../../colors";
+import { ChevronRight, Delete, Lock, LogOut, Trash, User } from "react-feather";
+
+const iconSize = 36;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 0;
+`;
+
+const Header = styled.header`
+  width: 100%;
+  background: ${COLORS.backgroundColor};
+  padding: 0.5em 1em;
+  font-size: 1.1em;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  background-image: linear-gradient(
+    270deg,
+    ${COLORS.primaryColor} 0%,
+    ${COLORS.lightPrimaryColor} 100%
+  );
+  -webkit-background-clip: text;
+  -moz-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  -moz-text-fill-color: transparent;
+  margin: 0;
+`;
+
+const Close = styled.a`
+  text-decoration: none;
+  color: ${COLORS.primaryColor};
+  font-weight: 500;
+  cursor: pointer;
+`;
+
+const Name = styled.p`
+  margin: 0.5em 0;
+  font-size: 2em;
+  font-weight: 500;
+`;
+
+const List = styled.ul`
+  width: 100%;
+  list-style: none;
+  margin: 0;
+  padding: 0.5em 1em;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ListElement = styled.li`
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  margin: 0.25em 0;
+  padding: 0.25em 0.5em;
+  border-radius: 2em;
+  cursor: pointer;
+  &:first-child {
+    margin-top: 0;
+  }
+  &:last-child {
+    margin-bottom: 0;
+  }
+  &:hover {
+    background-color: ${COLORS.hoverColor};
+  }
+`;
+
+const SectionTitle = styled.p`
+  color: ${COLORS.black063};
+  margin: 0;
+  padding: 0.25em 0.5em;
+  font-size: 1.25em;
+`;
+
+const Option = styled.span`
+  font-weight: 500;
+  margin-left: 0.75em;
+  font-size: 1.25em;
+`;
+
+const LogOutIcon = styled(LogOut)`
+  width: ${iconSize}px;
+  height: ${iconSize}px;
+  color: ${COLORS.backgroundColor};
+  border-radius: 100%;
+  background-color: ${COLORS.neutralColorIcon};
+  padding: 0.25em;
+`;
+
+const PersonIcon = styled(User)`
+  width: ${iconSize}px;
+  height: ${iconSize}px;
+  color: ${COLORS.backgroundColor};
+  border-radius: 100%;
+  background-color: ${COLORS.accountColorIcon};
+  padding: 0.25em;
+`;
+
+const LockIcon = styled(Lock)`
+  width: ${iconSize}px;
+  height: ${iconSize}px;
+  color: ${COLORS.backgroundColor};
+  border-radius: 100%;
+  background-color: ${COLORS.accountColorIcon};
+  padding: 0.25em;
+`;
+
+const DeleteIcon = styled(Trash)`
+  width: ${iconSize}px;
+  height: ${iconSize}px;
+  color: ${COLORS.backgroundColor};
+  border-radius: 100%;
+  background-color: ${COLORS.deleteColorIcon};
+  padding: 0.25em;
+`;
+
+const ChevronRightIcon = styled(ChevronRight)`
+  margin-left: auto;
+`;
+
+const Styled = {
+  Container,
+  Header,
+  Title,
+  Close,
+  Name,
+  List,
+  ListElement,
+  Option,
+  SectionTitle,
+  LogOutIcon,
+  PersonIcon,
+  LockIcon,
+  DeleteIcon,
+  ChevronRightIcon,
+};
+
+export default Styled;
