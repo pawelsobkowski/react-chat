140da3fb0ee9398390d671b4f818d7332ef8f412
Pawel Sobkowski <psobkowski98@gmail.com>
2021-01-05 21:24:16 +0100

    feat: show default value when photoUrl is not given

--- src/parts/Avatar/index.js
+++ src/parts/Avatar/index.js
@@ -5,16 +5,20 @@ const Avatar = ({ photoUrl, size }) => {
   const [isLoaded, setIsLoaded] = useState(false);
   return (
     <>
-      <Styled.Img
-        src={photoUrl}
-        alt="Avatar"
-        size={size && size}
-        isLoaded={isLoaded}
-        onLoad={() => {
-          setIsLoaded(true);
-        }}
-      />
-      {!isLoaded && <Styled.UserIcon size={size && size} />}
+      {photoUrl !== "" ? (
+        <Styled.Img
+          src={photoUrl}
+          alt="Avatar"
+          size={size && size}
+          isLoaded={isLoaded}
+          onLoad={() => {
+            setIsLoaded(true);
+          }}
+        />
+      ) : (
+        <Styled.UserIcon size={size && size} />
+      )}
+      {!isLoaded && photoUrl && <Styled.UserIcon size={size && size} />}
     </>
   );
 };
