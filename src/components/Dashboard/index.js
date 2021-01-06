043c04cd13c084e3c368daf4094d615b66e2b444
Pawel Sobkowski <psobkowski98@gmail.com>
2021-01-06 23:04:51 +0100

    feat: add function to display proper view

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -2,19 +2,41 @@ import React, { useState } from "react";
 import ChatRoom from "../ChatRoom";
 import Chats from "../Chats";
 import Styled from "./style";
+import Contacts from "../Contacts";
+import Profile from "../Profile";
 
 const Dashboard = () => {
   const [userToChat, setUserToChat] = useState(null);
   const [isActive, setIsActive] = useState(false);
+  const [sideView, setSideView] = useState("chats");
 
   const openChat = (id) => {
     setUserToChat(id);
     setIsActive(true);
   };
 
+  const sideSection = () => {
+    switch (sideView) {
+      case "chats":
+        return (
+          <Chats
+            openChat={(id) => openChat(id)}
+            changeView={(name) => setSideView(name)}
+          />
+        );
+      case "contacts":
+        return <Contacts changeView={(name) => setSideView(name)} />;
+      case "profile":
+        return <Profile changeView={(name) => setSideView(name)} />;
+      default:
+        return "";
+    }
+  };
+
   return (
     <Styled.Container>
-      <Chats openChat={(id) => openChat(id)} />
+      <Styled.SideSection>{sideSection()}</Styled.SideSection>
+
       {userToChat && isActive ? (
         <ChatRoom
           userToChat={userToChat}
