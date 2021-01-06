33f93322b80210c98bf7151ae59ddb89b062702a
Pawel Sobkowski <psobkowski98@gmail.com>
2021-01-06 23:02:12 +0100

    refactor: remove navlink and add function to change side view

--- src/parts/Navigation/index.js
+++ src/parts/Navigation/index.js
@@ -1,25 +1,18 @@
 import React from "react";
 import Styled from "./style";
-import { NavLink } from "react-router-dom";
 
-const Navigation = () => {
+const Navigation = ({ changeView }) => {
   return (
     <Styled.Navigation>
       <Styled.List>
-        <Styled.Element>
-          <NavLink to="/chats">
-            <Styled.ChatsIcon />
-          </NavLink>
+        <Styled.Element onClick={() => changeView("chats")}>
+          <Styled.ChatsIcon />
         </Styled.Element>
-        <Styled.Element>
-          <NavLink to="/contacts">
-            <Styled.PeopleIcon />
-          </NavLink>
+        <Styled.Element onClick={() => changeView("contacts")}>
+          <Styled.PeopleIcon />
         </Styled.Element>
-        <Styled.Element>
-          <NavLink to="/profile">
-            <Styled.AvatarIcon />
-          </NavLink>
+        <Styled.Element onClick={() => changeView("profile")}>
+          <Styled.AvatarIcon />
         </Styled.Element>
       </Styled.List>
     </Styled.Navigation>
